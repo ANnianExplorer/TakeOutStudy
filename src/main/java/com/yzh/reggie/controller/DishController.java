4472c4724433c53c779ca338ba34446d7d4360d1
ANnianbug <3480088617@qq.com>
2023-01-14 15:43:24 +0800

    瑞吉外卖-菜品分页查询+修改菜品等

--- src/main/java/com/yzh/reggie/controller/DishController.java
+++ src/main/java/com/yzh/reggie/controller/DishController.java
@@ -1,17 +1,22 @@
 package com.yzh.reggie.controller;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.fasterxml.jackson.databind.util.BeanUtil;
 import com.yzh.reggie.common.R;
 import com.yzh.reggie.dto.DishDto;
+import com.yzh.reggie.entity.Category;
 import com.yzh.reggie.entity.Dish;
+import com.yzh.reggie.service.CategoryService;
 import com.yzh.reggie.service.DishFlavorService;
 import com.yzh.reggie.service.DishService;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.BeanUtils;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
+import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * 菜品控制器
@@ -28,6 +33,8 @@ public class DishController {
     private DishService dishService;
     @Resource
     private DishFlavorService dishFlavorService;
+    @Resource
+    private CategoryService categoryService;
 
     /**
      *  新增菜品
@@ -39,5 +46,79 @@ public class DishController {
         log.info(dishDto.toString());
         dishService.saveWithFlavor(dishDto);
         return R.success("新增菜品成功！");
+    }
+
+    /**
+     * 分页查询
+     * @param page
+     * @param pageSize
+     * @param name
+     * @return
+     */
+    @GetMapping("/page")
+    public R<Page> page(int page,int pageSize,String name){
+        // 分页构造器
+        Page<Dish> pageInfo = new Page<>(page,pageSize);
+        // 这里因为需要有categoryName这一属性，
+        // 所以需要引入DishDto，所以需要进行copy操作
+        Page<DishDto> dishDtoPage = new Page<>(page,pageSize);
+
+
+        // 条件构造器
+        LambdaQueryWrapper<Dish> dishLambdaQueryWrapper = new LambdaQueryWrapper<>();
+        dishLambdaQueryWrapper
+                .like(name != null,Dish::getName,name)
+                .orderByDesc(Dish::getUpdateTime);
+
+
+        dishService.page(pageInfo,dishLambdaQueryWrapper);
+        // 对象拷贝
+        BeanUtils.copyProperties(pageInfo,dishDtoPage,"records");
+        List<Dish> records = pageInfo.getRecords();
+
+        List<DishDto> list = records.stream().map((item) -> {
+            DishDto dishDto = new DishDto();
+            // 拷贝忽略records
+            BeanUtils.copyProperties(item,dishDto);
+
+            // 得到分类id
+            Long categoryId = item.getCategoryId();
+            // 得到分类名称
+            Category category = categoryService.getById(categoryId);
+            if (category != null){
+                dishDto.setCategoryName(category.getName());
+            }
+
+            return dishDto;
+        }).collect(Collectors.toList());
+
+        dishDtoPage.setRecords(list);
+
+        return R.success(dishDtoPage);
+    }
+
+    /**
+     * 根据id修改菜品信息和口味信息
+     * @param id
+     * @return
+     */
+    @GetMapping("/{id}")
+    public R<DishDto> update(@PathVariable Long id){
+        DishDto dishDto = dishService.getByIdWithFlavor(id);
+
+        return R.success(dishDto);
+    }
+
+    /**
+     * 保存更新
+     *
+     * @param dishDto 菜dto
+     * @return {@link R}<{@link String}>
+     */
+    @PutMapping()
+    public R<String> saveUpdate(@RequestBody DishDto dishDto){
+        dishService.updateWithFlavor(dishDto);
+
+        return R.success("菜品修改成功！");
     }
 }
--- src/main/java/com/yzh/reggie/service/DishService.java
+++ src/main/java/com/yzh/reggie/service/DishService.java
@@ -9,4 +9,12 @@ public interface DishService extends IService<Dish> {
     // 新增菜品，同时插入菜品对于的口味数据，需要操作两张表
     public void saveWithFlavor(DishDto dishDto);
 
+    public DishDto getByIdWithFlavor(Long id);
+
+    /**
+     * 更新菜品和口味
+     *
+     * @param dishDto 菜dto
+     */
+    public void updateWithFlavor(DishDto dishDto);
 }
