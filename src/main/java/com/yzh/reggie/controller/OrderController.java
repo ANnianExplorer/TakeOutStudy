421e694f2d8be47ab47a6faadb5449e7f8cc3a3f
ANnianbug <3480088617@qq.com>
2023-01-15 17:23:27 +0800

    瑞吉外卖优化-移动端地址簿+菜品展示+购物车+用户下单等

--- src/main/java/com/yzh/reggie/controller/OrderController.java
+++ src/main/java/com/yzh/reggie/controller/OrderController.java
@@ -1,9 +1,9 @@
 package com.yzh.reggie.controller;
 
 import com.yzh.reggie.common.R;
-import com.yzh.reggie.entity.OrderDetail;
+import com.yzh.reggie.entity.Orders;
+import com.yzh.reggie.service.OrderService;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.core.annotation.Order;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,17 +21,17 @@ import javax.annotation.Resource;
 public class OrderController {
 
     @Resource
-    private Order order;
+    private OrderService orderService;
 
     /**
      * 提交
      *
-     * @param order 订单
+     * @param orders 订单
      * @return {@link R}<{@link String}>
      */
     @PostMapping("/submit")
-    public R<String> submit(@RequestBody Order order){
-
-
+    public R<String> submit(@RequestBody Orders orders){
+        orderService.submit(orders);
+        return R.success("下单成功！");
     }
 }
